5b99a2718586d3d26e68ec12bd8d7c4566912d34
hernancete <hernangiayetto@gmail.com>
2025-04-03 14:03:26 -0300

    feat(delete): add delete user route

--- src/adapters/routes.ts
+++ src/adapters/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { findAllController } from './findAllController';
 import { createUserController } from './createUserController';
+import { deleteUserController } from './deleteUserController';
 
 const router = express.Router();
 
 router.get('/users', findAllController);
 router.post('/users', createUserController);
+router.delete('/users/:wallet_id', deleteUserController);
 
 export default router;
