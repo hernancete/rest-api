4e03e622dd0a419995797f3137bd83606b3d4d5c
hernancete <hernangiayetto@gmail.com>
2025-04-03 13:30:49 -0300

    test(create): test calling the service create with full user data

--- test/application/createUserUseCase.test.ts
+++ test/application/createUserUseCase.test.ts
@@ -3,9 +3,10 @@ import { FileUserRepository } from '../../src/infrastructure/fileUserRepository'
 
 describe('Create user use case', () => {
 
-  test('It should set a new wallet_id for the user', async () => {
-    const uuidRegex = new RegExp(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
+  const uuidRegex = new RegExp(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
+  const isoDateRegex = new RegExp(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?Z$/);
 
+  test('It should set a new wallet_id for the user', async () => {
     const newUser = {
       "email": "newuser@gmail.com",
       "name": "Juan",
@@ -23,7 +24,6 @@ describe('Create user use case', () => {
 
     expect(spy).toHaveBeenCalledTimes(1);
     expect(spy).toHaveBeenCalledWith(expect.objectContaining({
-      ...newUser,
       wallet_id: expect.stringMatching(uuidRegex),
     }));
 
@@ -31,8 +31,30 @@ describe('Create user use case', () => {
   });
 
   test('It should set a the created_at for the user', async () => {
-    const isoDateRegex = new RegExp(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?Z$/);
+    const newUser = {
+      "email": "newuser@gmail.com",
+      "name": "Juan",
+      "last_name": "Perez",
+      "sex_type": "male",
+      "dni": "81234567",
+      "birth_date": "1990-06-12T10:41:19.886Z",
+    };
 
+    const userRepository = new FileUserRepository();
+    const spy = jest.spyOn(userRepository, 'create').mockResolvedValue({} as never);
+
+    const createUserUseCase = new CreateUserUseCase(userRepository);
+    await createUserUseCase.execute(newUser);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(expect.objectContaining({
+      created_at: expect.stringMatching(isoDateRegex),
+    }));
+
+    spy.mockRestore();
+  });
+
+  test('It should call the service create action with the provided data plus the generated one', async () => {
     const newUser = {
       "email": "newuser@gmail.com",
       "name": "Juan",
@@ -52,6 +74,7 @@ describe('Create user use case', () => {
     expect(spy).toHaveBeenCalledWith(expect.objectContaining({
       ...newUser,
       created_at: expect.stringMatching(isoDateRegex),
+      wallet_id: expect.stringMatching(uuidRegex),
     }));
 
     spy.mockRestore();
