c4f1d43f8f9daa75ecc652daac7e774c7b482edc
hernancete <hernangiayetto@gmail.com>
2025-04-03 22:52:27 -0300

    feat(update): update user use case

---
+++ src/application/updateUserUseCase.ts
@@ -0,0 +1,27 @@
+import { UserRepositoryInterface } from "./userRepositoryInterface";
+import { User } from '../domain/user';
+import { InvalidInputError } from '../domain/errors';
+
+export class UpdateUserUseCase {
+
+  constructor(private userRepository: UserRepositoryInterface) {}
+
+  async execute(user: Partial<User>): Promise<User> {
+
+    if (!user.wallet_id) throw new InvalidInputError('Invalid wallet_id');
+
+    const updateUser = {
+      wallet_id: user.wallet_id,
+      ...(user.email ? { email: user.email } : {}),
+      ...(user.name ? { name: user.name } : {}),
+      ...(user.last_name ? { last_name: user.last_name } : {}),
+      ...(user.sex_type ? { sex_type: user.email } : {}),
+      ...(user.dni ? { dni: user.dni } : {}),
+      ...(user.birth_date ? { birth_date: user.birth_date } : {}),
+    }
+
+    const updated = await this.userRepository.update(updateUser);
+    return updated;
+  }
+
+}
---
+++ test/application/updateUserUseCase.test.ts
@@ -0,0 +1,51 @@
+import { UpdateUserUseCase } from '../../src/application/updateUserUseCase';
+import { FileUserRepository } from '../../src/infrastructure/fileUserRepository';
+
+describe('Update user use case', () => {
+
+  test('It should throw an error if missing wallet_id', async () => {
+    const updateUser = {
+      "name": "Juan",
+      "last_name": "Perez",
+    };
+
+    const userRepository = new FileUserRepository();
+    const spy = jest.spyOn(userRepository, 'update').mockResolvedValue({} as never);
+
+    const updateUserUseCase = new UpdateUserUseCase(userRepository);
+    await expect(updateUserUseCase.execute(updateUser)).rejects.toThrow();
+    expect(spy).toHaveBeenCalledTimes(0);
+
+    spy.mockRestore();
+  });
+
+  test('Only pass to the user repository the updatable fields', async () => {
+
+    const updateUser = {
+      "wallet_id": "ebfd85bb-757b-4021-a7af-5a723d905cf2",
+      "name": "Juan",
+      "last_name": "Perez",
+      "created_at": "1990-06-12T10:41:19.886Z",
+      "another_attr": "invalid attr",
+    };
+
+    const userRepository = new FileUserRepository();
+    const spy = jest.spyOn(userRepository, 'update').mockResolvedValue({} as never);
+
+    const updateUserUseCase = new UpdateUserUseCase(userRepository);
+    updateUserUseCase.execute(updateUser);
+
+    const [userToUpdate] = spy.mock.calls[0];
+
+    expect(userToUpdate).toEqual(expect.objectContaining({
+      "wallet_id": "ebfd85bb-757b-4021-a7af-5a723d905cf2",
+      "name": "Juan",
+      "last_name": "Perez",
+    }));
+    expect(userToUpdate).not.toHaveProperty('created_at');
+    expect(userToUpdate).not.toHaveProperty('another_attr');
+
+    spy.mockRestore();
+  });
+
+});
