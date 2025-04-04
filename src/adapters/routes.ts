e73fd99e8a9a03ddaa914c97f962cde96157907f
hernancete <hernangiayetto@gmail.com>
2025-04-04 15:18:57 -0300

    feat(update): add udpate user route

--- src/adapters/routes.ts
+++ src/adapters/routes.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { findAllController } from './controllers/findAllController';
 import { createUserController } from './controllers/createUserController';
 import { deleteUserController } from './controllers/deleteUserController';
+import { updateUserController } from './controllers/updateUserController';
 
 const router = express.Router();
 
 router.get('/users', findAllController);
 router.post('/users', createUserController);
+router.put('/users/:wallet_id', updateUserController);
 router.delete('/users/:wallet_id', deleteUserController);
 
 export default router;
