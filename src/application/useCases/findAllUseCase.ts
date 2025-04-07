af1cba652ba71a663c21b5c2f0cd57361edc4f3f
hernancete <hernangiayetto@gmail.com>
2025-04-07 19:57:29 -0300

    feat(filters): curate pagination filters in use case

--- src/application/useCases/findAllUseCase.ts
+++ src/application/useCases/findAllUseCase.ts
@@ -1,12 +1,29 @@
-import { UserRepositoryInterface } from "../userRepositoryInterface";
+import { Filters, UserRepositoryInterface } from "../userRepositoryInterface";
 import { User } from '../../domain/user';
+import { InvalidInputError } from '../../domain/errors';
+
 
 export class FindAllUseCase {
 
   constructor(private userRepository: UserRepositoryInterface) {}
 
-  async execute(): Promise<User[]> {
-    return this.userRepository.findAll();
+  async execute(filters?: any): Promise<User[]> {
+
+    const curatedFilters: Filters = {};
+    if (filters && filters.page && !isNaN(parseInt(filters.page))) {
+      if (parseInt(filters.page) < 0) {
+        throw new InvalidInputError('Invalid parameter');
+      }
+      curatedFilters.page = parseInt(filters.page);
+    }
+    if (filters && filters.limit && !isNaN(parseInt(filters.limit))) {
+      if (parseInt(filters.limit) < 0) {
+        throw new InvalidInputError('Invalid parameter');
+      }
+      curatedFilters.limit = parseInt(filters.limit);
+    }
+
+    return this.userRepository.findAll(curatedFilters);
   }
 
 }
--- test/application/useCases/findAllUseCase.test.ts
+++ test/application/useCases/findAllUseCase.test.ts
@@ -12,7 +12,66 @@ describe('Find all use case', () => {
     const result = await findAllUseCase.execute();
 
     expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenCalledWith();
+    expect(spy).toHaveBeenCalledWith({});
+
+    spy.mockRestore();
+  });
+
+  test('It should execute the service with curated pagination filters: strings', async () => {
+
+    const filters = {
+      page: '1',
+      limit: '4',
+    };
+    const filtersCurated = {
+      page: 1,
+      limit: 4,
+    }
+
+    const userRepository = new FileUserRepository();
+    const spy = jest.spyOn(userRepository, 'findAll').mockResolvedValue('' as never);
+
+    const findAllUseCase = new FindAllUseCase(userRepository);
+    const result = await findAllUseCase.execute(filters);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(filtersCurated);
+
+    spy.mockRestore();
+  });
+
+  test('It should throw an error if invalid filters are provided: page out of range', async () => {
+
+    const filters = {
+      page: '-1',
+      limit: '4',
+    };
+
+    const userRepository = new FileUserRepository();
+    const spy = jest.spyOn(userRepository, 'findAll').mockResolvedValue('' as never);
+
+    const findAllUseCase = new FindAllUseCase(userRepository);
+
+    await expect(findAllUseCase.execute(filters)).rejects.toThrow()
+    expect(spy).toHaveBeenCalledTimes(0);
+
+    spy.mockRestore();
+  });
+
+  test('It should throw an error if invalid filters are provided: limit out of range', async () => {
+
+    const filters = {
+      page: '1',
+      limit: '-4',
+    };
+
+    const userRepository = new FileUserRepository();
+    const spy = jest.spyOn(userRepository, 'findAll').mockResolvedValue('' as never);
+
+    const findAllUseCase = new FindAllUseCase(userRepository);
+
+    await expect(findAllUseCase.execute(filters)).rejects.toThrow()
+    expect(spy).toHaveBeenCalledTimes(0);
 
     spy.mockRestore();
   });
