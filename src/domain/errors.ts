8bf6d78dc5d71759fe6136ca24fbe1874cbdd428
hernancete <hernangiayetto@gmail.com>
2025-04-08 18:42:15 -0300

    feat(error-handling): add error types

--- src/domain/errors.ts
+++ src/domain/errors.ts
@@ -1,1 +1,15 @@
-export class InvalidInputError extends Error { };
+export class InvalidInputError extends Error {
+  status: number;
+  constructor(message: string = 'Bad request') {
+    super(message);
+    this.status = 400;
+  }
+};
+
+export class UnauthorizedError extends Error {
+  status: number;
+  constructor(message: string = 'Unauthorized') {
+    super(message);
+    this.status = 401;
+  }
+};
