e026289bae0a025aaafa168346facde95f37850b
hernancete <hernangiayetto@gmail.com>
2025-04-08 18:44:31 -0300

    fix(error-handling): improve the error handler middleware

--- src/adapters/middlewares/errorHandlerMiddleware.ts
+++ src/adapters/middlewares/errorHandlerMiddleware.ts
@@ -1,6 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
+import { UnauthorizedError, InvalidInputError } from '../../domain/errors';
 
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-  res.status(500);
-  res.json({ message: 'error' });
+  if (err instanceof UnauthorizedError) {
+    res.status(err.status || 401);
+    res.json({ message: err.message });
+  } else if (err instanceof InvalidInputError) {
+    res.status(err.status || 400);
+    res.json({ message: err.message });
+  } else {
+    res.status(500);
+    res.json({ message: 'Internal error' });
+  }
 }
